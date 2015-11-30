177e7da98ca93ba4960491611d95fcf3e6fb9e18
Simon Holloway <simon@syhol.io>
2015-11-30 14:41:43 +0000

    setters

--- Entity.php
+++ Entity.php
@@ -6,7 +6,9 @@ class Entity
         'first_name' => 'required|string|min:2|max:30|alphaspace',
         'last_name' => 'required|string|min:2|max:30|alphaspace',
         'full_name' => 'read_only|from:getFullName',
-        'age' => 'required|int|min:18|max:200',
+        'dob' => 'required|date|min:01/01/1900|max:now -10 years',
+        'age' => 'read_only|from:getAge',
+        'canBuyAlcohol' => 'read_only|from:canBuyAlcohol',
         'price' => 'required|price',
         'children' => 'array',
         'children.*.name' => 'required|string|min:2|max:30|alphaspace'
@@ -15,5 +17,15 @@ class Entity
     public function getFullName()
     {
         return $this->get('first_name') . ' ' . $this->get('last_name');
+    }
+    
+    public function getAge()
+    {
+        return (new DateTime())->format('y') - $this->get('dob')->format('Y');
+    }
+    
+    public function canBuyAlcohol()
+    {
+        return $this->get('age') >= 18;
     }
 }
