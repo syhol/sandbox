03f10ad1c73ce346b1ef341ac608639e6656e6b7
Simon Holloway <simon@syhol.io>
2016-08-14 00:08:07 +0100

    Update functional.php

--- functional.php
+++ functional.php
@@ -2,52 +2,124 @@
 
 namespace Prelude;
 
-function map(callable $callable, $array) {
-    return array_map($callable, $array);
+use Closure;
+use Exception;
+use Generator;
+use ReflectionFunction;
+use ReflectionMethod;
+
+function ary($collection) {
+    if (is_array($collection)) {
+        return $collection;
+    } elseif (is_string($collection)) {
+        return str_split($collection);
+    } elseif ($collection instanceof Arrayable) {
+        return $collection->toArray();
+    } elseif ($collection instanceof \Generator) {
+        $array = [];
+        while ($collection->valid()) {
+            $array[] = $collection->current();
+            $collection->next();
+        }
+        return $array;
+    } elseif ($collection instanceof \Iterator) {
+        return iterator_to_array($collection);
+    }
+
+    throw new Exception('Could not convert to array');
 }
 
-function filter(callable $callable, $array) {
-    return array_filter($array, $callable);
+function col($collection, array $array) {
+    if (is_string($collection)) {
+        return implode('', $array);
+    } elseif ($collection instanceof Arrayable) {
+        return $collection->fromArray($array);
+    }
+
+    return $array;
+}
+
+function append($item, $collection) {
+    $array = ary($collection);
+    array_push($array, $item);
+    return col($collection, $array);
+}
+
+function prepend($item, $collection) {
+    $array = ary($collection);
+    array_unshift($array, $item);
+    return col($collection, $array);
+}
+
+function foldr(callable $callable, $initial, $collection) {
+    foreach (ary($collection) as $item) {
+        $initial = $callable($item, $initial);
+    }
+    return $initial;
+}
+
+function foldl(callable $callable, $initial, $array) {
+    return foldr(flip($callable), $initial, $array);
+}
+
+function map(callable $callable, $collection) {
+    $array = ary($collection);
+    foreach ($array as $key => $value) {
+        $array[$key] = $callable($value);
+    }
+    return col($collection, $array);
+}
+
+function filter(callable $callable, $collection) {
+    $array = [];
+    foreach ($collection as $key => $value) {
+        if($callable($value)) $array[$key] = $value;
+    }
+    return col($collection, $array);
 }
 
 function head($array) {
-    return array_slice($array, 0, 1);
+    return take(1, $array);
 }
 
 function last($array) {
-    return array_slice($array, count($array) - 2, 1);
+    return take(-1, $array);
 }
 
 function tail($array) {
-    return array_slice($array, 1);
+    return drop(1, $array);
 }
 
 function init($array) {
-    return array_slice($array, 0, count($array) - 2);
+    return drop(-1, $array);
 }
 
 function reverse($array) {
-    return array_reverse($array);
+    return foldr('Prelude\prepend', [], $array);
 }
 
-function concat($array) {
-    return call_user_func_array('array_merge', $array);
+function concat($collection) {
+    return col($collection, array_merge(...ary($collection)));
 }
 
 function any(callable $callable, $array) {
-    return count(array_filter($array, $callable)) > 1;
+    return count(filter($callable, $array)) > 0;
 }
 
 function all(callable $callable, $array) {
-    return count(array_filter($array, $callable)) === count($array);
+    return count(filter($callable, $array)) === count($array);
 }
 
-function not($item) {
-    return ! $item;
+function contains($item, $iterator) {
+    return any(partial('Prelude\equals', $item), $iterator);
+}
+
+function equals($a, $b) {
+    return $a === $b;
 }
 
 function null($item) {
-    return empty($item);
+    return ! $item;
 }
 
 function id($item) {
@@ -58,49 +130,71 @@ function constant($item) {
     return function() use ($item) { return $item; };
 }
 
+function insert($index, $value, $array) {
+    return $array[$index] = $value;
+}
+
+function exists($index, $array) {
+    return isset($array[$index]);
+}
+
+function delete($index, $array) {
+    unset($array[$index]);
+    return $array;
+}
+
 function pick($index, $array) {
     return $array[$index];
 }
 
-function pluck($index, $array) {
-    return array_map(function($item) use ($index) { return $item[$index]; }, $array);
+function pluck($index, $collection) {
+    return map(partial('pick', $index), $collection);
 }
 
-function take($size, $array) {
-    return array_slice($array, 0, $size);
+function take($size, $collection) {
+    if ($collection instanceof Generator) {
+        if ($size < 0) throw new Exception('Can\'t take items from end of Generators');
+        $count = 0;
+        $array = [];
+        while ($count++ < $size) {
+            $collection->next();
+            $array[] = $collection->current();
+        }
+        return $array;
+    }
+
+    return $size > 0
+        ? col($collection, array_slice(ary($collection), 0, $size))
+        : col($collection, array_slice(ary($collection), $size));
 }
 
-function drop($size, $array) {
-    return array_slice($array, $size);
+function drop($size, $collection) {
+    return $size > 0
+        ? col($collection, array_slice(ary($collection), $size))
+        : col($collection, array_slice(ary($collection), 0, $size));
 }
 
-function partial_left($param, callable $callable) {
-    return function () use($param, $callable) {
-        return call_user_func_array($callable, array_merge([$param], func_get_args()));
-    };
-}
-
-function partial_right($param, callable $callable) {
-    return function () use($param, $callable) {
-        return call_user_func_array($callable, array_merge(func_get_args(), [$param]));
-    };
-}
-
-function partial_at($index, $param, callable $callable) {
-    return function () use($index, $param, $callable) {
-        return call_user_func_array($callable, array_merge(func_get_args(), [$param]));
-    };
-}
-
-function zip() {
+function zip(...$arrays) {
     $zipped = [];
-    $matrix = map(partial_left('take', max(map('length', func_get_args()))), func_get_args());
-  while(!all('null', $matrix)) {
-      $zipped[] = map('head', filter(pipe('not', 'null'), $matrix));
-      $matrix = map('tail', $matrix);
-  }
-  return $zipped;
+    $matrix = map(partial('Prelude\take', min(map('count', $arrays))), $arrays);
+    while(!all('Prelude\not', $matrix)) {
+        $zipped[] = concat(map('Prelude\head', $matrix));
+        $matrix = map('Prelude\tail', $matrix);
+    }
+    return $zipped;
 }
+
+function map_to_tuple($array) {
+    return zip(array_keys(ary($array)), ary($array));
+}
+
+function tuple_to_map($array) {
+    $keys = concat(map('Prelude\head', $array));
+    $values = concat(map('Prelude\last', $array));
+    return array_combine($keys, $values);
+}
+
+// String
 
 function chars($chars) {
     return str_split($chars);
@@ -126,8 +220,117 @@ function unlines($lines) {
     return implode(PHP_EOL, $lines);
 }
 
+// Infinite List
+
+function times(callable $callable, $size) {
+    $count = 0;
+    while ($count++ < $size) yield $callable();
+}
+
+function iterate(callable $callable, $initial) {
+    while(true) yield $initial = $callable($initial);
+}
+
+function repeat($item) {
+    while (true) yield $item;
+}
+
+function replicate($item, $size) {
+    $count = 0;
+    while ($count++ < $size) yield $item;
+}
+
+function cycle($list) {
+    while (true) foreach ($list as $item) yield $item;
+}
+
+// PHP Specials
+
+function compare_ord(Ord $a, Ord $b) {
+    return $a->compare($b);
+}
+
+function flip(callable $callable) {
+    return function(...$params) use ($callable) {
+        return $callable(...array_reverse($params));
+    };
+}
+
+function splat(callable $callable) {
+    return function($params) use ($callable) {
+        return $callable(...$params);
+    };
+}
+
+function unsplat(callable $callable) {
+    return function(...$params) use ($callable) {
+        return $callable($params);
+    };
+}
+
+function compose(callable $callable1, callable $callable2) {
+    return function(...$params) use ($callable1, $callable2) {
+        return $callable1($callable2(...$params));
+    };
+}
+
+function partial(callable $callable, ...$params) {
+    return function (...$more) use($params, $callable) {
+        return $callable(...array_merge($params, $more));
+    };
+}
+
+function apply(callable $callable, ...$params) {
+    return $callable(...$params);
+}
+
+function method($method) {
+    return function ($object) use($method) {
+        return [$object, $method];
+    };
+}
+
+function reflect_callable(callable $callable) {
+    $callable = (is_string($callable) && strpos($callable, '::') !== false)
+        ? explode('::', $callable, 2)
+        : $callable;
+
+    if (is_array($callable) && count($callable) === 2) {
+        list($class, $method) = array_values($callable);
+
+        if (is_string($class) && ! method_exists($class, $method)) {
+            $method = '__callStatic';
+        }
+        if (is_object($class) && ! method_exists($class, $method)) {
+            $method = '__call';
+        }
+        return new ReflectionMethod($class, $method);
+    } elseif ($callable instanceof Closure || is_string($callable)) {
+        return new ReflectionFunction($callable);
+    } elseif (is_object($callable) && method_exists($callable, '__invoke')) {
+        return new ReflectionMethod($callable, '__invoke');
+    }
+
+    throw new Exception('Could not parse function');
+}
+
+function curry(callable $callable, $count = null) {
+    $count = is_null($count) ? reflect_callable($callable)->getNumberOfRequiredParameters() : $count;
+    return $count === 0 ? $callable : function (...$params) use($callable, $count) {
+        $apply = partial($callable, ...$params);
+        return count($params) >= $count ? $apply() : curry($apply, $count - count($params));
+    };
+}
+
+class Foo { public function bar($name, $box) { echo $name . $box . PHP_EOL; } }
+
 // Interfaces
 
+interface Arrayable
+{
+    public function toArray();
+    public function fromArray(array $array);
+}
 interface Eq
 {
     public function equal(Eq $other);
@@ -194,7 +397,7 @@ interface Monad extends Applicative
 interface Foldable extends \Countable
 {
     public function fold();
-    public function foldMap(Monoid $monoid);
+    public function foldMap(callable $callable);
     public function foldr(callable $callable, $initial);
     public function foldl(callable $callable, $initial);
     public function null();
@@ -204,34 +407,4 @@ interface Foldable extends \Countable
     public function sum();
     public function product();
     public function toArray();
-}
-
-// PHP Specials
-
-function compare_ord(Ord $a, Ord $b) {
-    return $a->compare($b);
-}
-
-function flip(callable $callable) {
-    return function($params) use ($callable) {
-        return call_user_func_array($callable, array_reverse(func_get_args()));
-    };
-}
-
-function splat(callable $callable) {
-    return function($params) use ($callable) {
-        return call_user_func_array($callable, $params);
-    };
-}
-
-function unsplat(callable $callable) {
-    return function() use ($callable) {
-        return call_user_func($callable, func_get_args());
-    };
-}
-
-function pipe(callable $callable1, callable $callable2) {
-    return function() use ($callable2, $callable1) {
-        return $callable1(call_user_func_array($callable2, func_get_args()));
-    };
 }
